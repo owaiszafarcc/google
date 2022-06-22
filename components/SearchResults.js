7adf92a0edab9a0109811898ca0486c623bb3c0f
Owais Zafar <owais.zafar@hotmail.com>
2022-06-22 18:02:06 +0500

    Created Pagination Button

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Parser from 'html-react-parser'; 
+import Parser from 'html-react-parser';
+import PaginationButtons from './PaginationButtons'; 
 
 export default function SearchResults({results}){
     return (
@@ -19,6 +20,7 @@ export default function SearchResults({results}){
                 </div>
 
             ))}
+            <PaginationButtons />
 
         </div>
     );
